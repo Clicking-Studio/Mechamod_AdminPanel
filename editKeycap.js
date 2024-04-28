3d41a9be9172639b3c8b0fc2ce2729b4ec21a957
Mahmoud <tkhattab1999@gmail.com>
2024-04-28 01:02:25 -0400

    edit styling

--- editKeycap.js
+++ editKeycap.js
@@ -5,68 +5,73 @@ function openEditModal(id) {
         .then(keycap => {
             // Populate the edit form with keycap data
             const modalContent = `
-                <h2 class="text-lg font-semibold mb-2">Edit Keycap</h2>
-                <form id="editKeycapForm" class="space-y-2 grid grid-cols-2 gap-4">
+            <h2 class="text-lg font-semibold ">Edit Keycap</h2>
+            <form id="editKeycapForm" class="space-y-2 grid grid-cols-3 gap-4">
+                <div class="col-span-1">
+                    <label for="editName" class="mt-2 block font-small text-gray-700">Name:</label>
                     <input type="hidden" id="editKeycapId" value="${keycap.keycap_id}">
-                    <div class="col-span-1">
-                        <label for="editName" class="block font-small text-gray-700">Name:</label>
-                        <input type="text" id="editName" name="editName" value="${keycap.name}" required
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
-                        <label for="editPrice" class="block font-small text-gray-700">Price:</label>
-                        <input type="number" id="editPrice" name="editPrice" value="${keycap.price}" required
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
-                        <label for="editOrderPosition" class="block font-small text-gray-700">Order Position:</label>
-                        <input type="number" id="editOrderPosition" name="editOrderPosition" value="${keycap.order_position}" required
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                    <input type="text" id="editName" name="editName" value="${keycap.name}" required
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                    <label for="editPrice" class="block font-small text-gray-700">Price:</label>
+                    <input type="number" id="editPrice" name="editPrice" value="${keycap.price}" required
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                    <label for="editOrderPosition" class="block font-small text-gray-700">Order Position:</label>
+                    <input type="number" id="editOrderPosition" name="editOrderPosition" value="${keycap.order_position}" required
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                </div>
+                <div class="col-span-1">
+                    <label for="editDescription" class="block font-small text-gray-700">Description:</label>
+                    <textarea id="editDescription" name="editDescription" rows="4" required
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">${keycap.description}</textarea>
+                    <label for="editBullet1" class="block font-small text-gray-700">Bullet 1:</label>
+                    <input type="text" id="editBullet1" name="editBullet1" value="${keycap.bullet1 || ''}"
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                    <label for="editBullet2" class="block font-small text-gray-700">Bullet 2:</label>
+                    <input type="text" id="editBullet2" name="editBullet2" value="${keycap.bullet2 || ''}"
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                    <label for="editBullet3" class="block font-small text-gray-700">Bullet 3:</label>
+                    <input type="text" id="editBullet3" name="editBullet3" value="${keycap.bullet3 || ''}"
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                    <label for="editBullet4" class="block font-small text-gray-700">Bullet 4:</label>
+                    <input type="text" id="editBullet4" name="editBullet4" value="${keycap.bullet4 || ''}"
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                </div>
+                <div class="col-span-1">
+                    <div class="">
+                        <div class="mb-36">
                         <label for="editImage" class="block font-small text-gray-700">Image:</label>
                         <input type="file" id="editImage" name="editImage"
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                            class="w-full px-2 py-2 border border-gray-300 text-sm rounded-md focus:outline-none focus:border-blue-500">
+                        </div>
+                        <!-- Display existing image if available with smaller size -->
+                        <img id="existingImagePreview" src="${keycap.image_path}" class="mt-2 border border-gray-300 rounded-md ${keycap.image_path ? '' : 'hidden'} w-20 h-20 object-cover">
+                        <!-- Adjusted width (w-10) and height (h-10) for the image -->
+                    </div>
+                        
+                    <div class="">
                         <label for="editBackground" class="block font-small text-gray-700">Background:</label>
                         <input type="file" id="editBackground" name="editBackground"
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                            class="w-full px-2 py-2 border border-gray-300 text-sm rounded-md focus:outline-none focus:border-blue-500">
+                    
+                        <!-- Display existing background if available with smaller size -->
+                        <img id="existingBackgroundPreview" src="${keycap.background_path}" class="mt-2 border border-gray-300 rounded-md ${keycap.background_path ? '' : 'hidden'} w-20 h-20 object-cover">
+                        <!-- Adjusted width (w-10) and height (h-10) for the background -->
                     </div>
-                    <div class="col-span-1">
-                        <label for="editDescription" class="block font-small text-gray-700">Description:</label>
-                        <textarea id="editDescription" name="editDescription" rows="4" required
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">${keycap.description}</textarea>
-                        <label for="editBullet1" class="block font-small text-gray-700">Bullet 1:</label>
-                        <input type="text" id="editBullet1" name="editBullet1" value="${keycap.bullet1 || ''}"
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
-                        <label for="editBullet2" class="block font-small text-gray-700">Bullet 2:</label>
-                        <input type="text" id="editBullet2" name="editBullet2" value="${keycap.bullet2 || ''}"
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
-                        <label for="editBullet3" class="block font-small text-gray-700">Bullet 3:</label>
-                        <input type="text" id="editBullet3" name="editBullet3" value="${keycap.bullet3 || ''}"
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
-                        <label for="editBullet4" class="block font-small text-gray-700">Bullet 4:</label>
-                        <input type="text" id="editBullet4" name="editBullet4" value="${keycap.bullet4 || ''}"
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
-                    </div>
+                </div>
 
-                    <!-- Display existing image if available with smaller size -->
-                    <img id="existingImagePreview" src="${keycap.image_path}" class="mt-2 border border-gray-300 rounded-md ${keycap.image_path ? '' : 'hidden'} w-24 h-24 object-cover">
-                    <!-- Adjusted width (w-24) and height (h-24) for the image -->
-
-                    <!-- Display existing background if available with smaller size -->
-                    <img id="existingBackgroundPreview" src="${keycap.background_path}" class="mt-2 border border-gray-300 rounded-md ${keycap.background_path ? '' : 'hidden'} w-24 h-24 object-cover">
-                    <!-- Adjusted width (w-24) and height (h-24) for the background -->
-
-                    <!-- Buttons for saving changes and canceling -->
-                    <div class="col-span-2 flex justify-end space-x-4">
-                        <button type="submit"
-                            class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none">Save
-                            Changes</button>
-                        <button type="button"
-                            class="px-4 py-2 bg-gray-400 text-white rounded-md hover:bg-gray-500 focus:outline-none"
-                            onclick="closeEditModal()">Cancel</button>
-                    </div>
-                </form>
+            
+                <!-- Buttons for saving changes and canceling -->
+                <div class="col-span-3 flex justify-end space-x-4">
+                    <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none">Save Changes</button>
+                    <button type="button" class="px-4 py-2 bg-gray-400 text-white rounded-md hover:bg-gray-500 focus:outline-none" onclick="closeEditModal()">Cancel</button>
+                </div>
+            </form>
             `;
             document.querySelector('.modal-content').innerHTML = modalContent;
             document.getElementById('editKeycapModal').style.display = 'flex'; // Show the modal
 
             // Handle edit form submission
-            document.getElementById('editKeycapForm').addEventListener('submit', async function(event) {
+            document.getElementById('editKeycapForm').addEventListener('submit', async function (event) {
                 event.preventDefault();
 
                 // Get form values including bullet points
